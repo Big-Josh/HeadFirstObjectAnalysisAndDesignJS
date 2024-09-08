81b27cf6893926e766566bbbfadd7d62bf695bf0
Big Josh <otoojoshua616@gmail.com>
2024-09-08 23:57:26 +0000

    Dog gig

---
+++ DogDoor.js
@@ -0,0 +1,19 @@
+class DogDoor {
+    constructor() {
+        this.open = false; // Initially, the door is closed
+    }
+
+    openDoor() {
+        console.log("The dog door opens.");
+        this.open = true; // Change the state to open
+    }
+
+    closeDoor() {
+        console.log("The dog door closes.");
+        this.open = false; // Change the state to closed
+    }
+
+    isOpen() {
+        return this.open; // Return the current state of the door
+    }
+}
